c07e9970e8627ad8ac5245ef69a3e3caac276cab
saidone <saidone@saidone.org>
2026-03-29 17:28:24 +0200

    Remove password

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 JAVA_OPTS="-Xms64m -Xmx64m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:9000"
 export SPRING_PROFILES_ACTIVE=dev
 
-export SUPABASE_DB_PASSWORD=rEKPuAi9t375LcJJRdP7Fd9tpo7PZkTN
+export SUPABASE_DB_PASSWORD=
 
 mvn spring-boot:run -Dspring-boot.run.jvmArguments="$JAVA_OPTS"
